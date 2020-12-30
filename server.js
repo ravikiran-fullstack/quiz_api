2e84b2d0f950f350fd669ee4f4c332690dd8eac9
kiran-code <ravikiransjce.code@gmail.com>
2020-12-30 19:17:27 +0530

    Added code to check if a particular option in a specific question is correcto or not, also added code for incorrect question id or option id

--- server.js
+++ server.js
@@ -47,6 +47,32 @@ app.post("/question/:question_id", (req, res) => {
   }
 });
 
+app.post('/answer/:question_id/:option_id', (req, res) => {
+  console.log(req.params.question_id, req.params.option_id);
+  const questions = readJsonFile();
+  const questionIndex = questions.findIndex(q => q.id == req.params.question_id);
+  if(questionIndex === -1 ){
+    res.status(400).json({message: "Question not found"});
+  } else {
+    if(questions[questionIndex].options){
+      const optionIndex = questions[questionIndex].options.findIndex(option => option.id == req.params.option_id);
+      if(optionIndex === -1 ){
+        res.status(400).json({message: "Option not found"});
+      } else {
+        console.log('option value',questions[questionIndex].options[optionIndex]);
+        if(questions[questionIndex].options[optionIndex].isCorrect){
+          res.json({option: "Correct"});
+        } else {
+          res.json({option: "Wrong"});
+        }
+        
+      }
+    }
+  }
+  //console.log(questionIndex, questions[questionIndex]);
+  //res.json({question : questions[questionIndex]});
+})
+
 app.get("/questions", (req, res)=> {
   const questionsWithOptions = readJsonFile();
   const processedQuestions = processQuestionsWithOptions(questionsWithOptions);
