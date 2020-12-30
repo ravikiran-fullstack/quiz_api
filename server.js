d34b3c640d4761a930b5234df3f8c5a2844e7040
kiran-code <ravikiransjce.code@gmail.com>
2020-12-30 19:03:42 +0530

    Added code to post options to a partucalar question and mask the correct answer while retrieving a question

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.post("/question", (req, res) => {
     req.body.id = new Date().getTime();
     const questions = readJsonFile();
     questions.push(req.body);
-    const data = JSON.stringify(questions, null, 2);
-    fs.writeFileSync("questions.json",data);
+    writeJsonFile(questions);
 
     res.json({
       question: req.body
@@ -26,17 +25,39 @@ app.post("/question", (req, res) => {
   }
 });
 
+app.post("/question/:question_id", (req, res) => {
+  if(req.body.option === undefined || req.body.isCorrect === undefined){
+    res.status(400).json({message: "Please pass a valid option"});
+  } else {
+    const questions = readJsonFile();
+    const questionIndex = questions.findIndex(q => q.id == req.params.question_id);
+    if(questionIndex === -1) {
+      res.status(400).json({message: "Question not found"})
+    } else {
+      req.body.id = new Date().getTime();
+      if(questions[questionIndex].options){
+        questions[questionIndex].options.push(req.body);
+      } else {
+        questions[questionIndex].options = [];
+        questions[questionIndex].options.push(req.body);
+      }
+      res.json({options: questions[questionIndex].options});
+      writeJsonFile(questions);
+    }
+  }
+});
+
 app.get("/questions", (req, res)=> {
-  res.json(readJsonFile());
+  const questionsWithOptions = readJsonFile();
+  const processedQuestions = processQuestionsWithOptions(questionsWithOptions);
+  res.json(processedQuestions);
 });
 
 app.get("/question/:question_id", (req, res)=>{
-  // console.log(questions, req.params.question_id);
   const jsonQuestionsData = readJsonFile();
   const question = jsonQuestionsData.find(q => {
     return q.id == req.params.question_id;
   });
-  // console.log(question);
   res.json({question: question});
 })
 
@@ -44,4 +65,20 @@ function readJsonFile(){
   const rawQuestionsData = fs.readFileSync('questions.json');
   const jsonQuestionsData = JSON.parse(rawQuestionsData);
   return jsonQuestionsData;
+}
+
+function writeJsonFile(questions){
+  const data = JSON.stringify(questions, null, 2);
+  fs.writeFileSync("questions.json",data);
+}
+
+function processQuestionsWithOptions(questionsWithOptions){
+  
+  questionsWithOptions.forEach(q => {
+    console.log(q);
+    if(q.options){
+      q.options.forEach(option => option.isCorrect = null);
+    }
+  })
+  return questionsWithOptions;
 }
